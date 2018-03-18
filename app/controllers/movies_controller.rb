9327906c69f8b7dec12b1911fed131a91b16e096
Joao Marcos <joao.een@gmail.com>
2018-03-18 20:21:59 +0000

    Simple rotten potatoes app

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,33 @@ class MoviesController < ApplicationController
       id = params[:id]
       @movie = Movie.find(id)
     end
+    
+    def new
+        #default: render new template
+    end
+    
+    def create
+        @movie = Movie.create!(params.require(:movie).permit(:rating, :title, :release_date))
+        flash[:notice] = "#{@movie.title} was successfully create."
+        redirect_to movies_path
+    end
+
+    def edit
+        @movie = Movie.find params[:id]
+        
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(params.require(:movie).permit(:rating, :title, :release_date))
+        flash[:notice] = "#{@movie.title} was successfully update."
+        redirect_to movies_path
+    end
+    
+    def destroy
+        @movie = Movie.find params[:id]
+        @movie.destroy
+        flash[:notice] = "#{@movie.title} was successfully deleted."
+        redirect_to movies_path
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
-    
+    def self.all_ratings
+        ['PG', 'R', 'PG-13', 'PG-17']
+    end
 end
